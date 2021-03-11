552b528aefaef8c1c84df301678fde63e51f6149
quling <quling@dist.com.cn>
2021-03-11 23:16:28 +0800

    修改表单页的表单验证,已完成当不符合验证条件就提示的部分

--- miniprogram/pages/order/order.js
+++ miniprogram/pages/order/order.js
@@ -3,19 +3,30 @@
 var app = getApp();
 Page({
   data: {
-    photographyType: [
-      // { id: 13, name: "1室1厅1卫" },
-      // { id: 14, name: "1室2厅1卫" },
-      // { id: 15, name: "2室1厅1卫" },
-      // { id: 16, name: "3室1厅2卫" },
-      // { id: 17, name: "4室1厅2卫" },
-      // { id: 18, name: "5室1厅3卫" },
-      // { id: 19, name: "6室1厅3卫" },
-      // { id: 20, name: "7室以上" },
-    ],
-    hx_index: null,
-    name: "",
-    time: "",
+    photographyType: [],
+    index: null,
+    formData: {
+      name: {
+        value: "",
+        isError: false,
+        massage: "请输入联系人",
+      },
+      time: {
+        value: "",
+        isError: false,
+        massage: "请选择服务时间",
+      },
+      server: {
+        value: "",
+        isError: false,
+        massage: "请选择服务项目",
+      },
+      phone: {
+        value: "",
+        isError: false,
+        massage: "请输入联系电话",
+      },
+    },
   },
   // 生命周期
   onLoad() {
@@ -39,10 +50,10 @@ Page({
     }
     this.setData({
       photographyType: type,
-      name: app.globalData.nickName,
+      "formData.name.value": app.globalData.nickName,
     });
+    console.log("初始化", this.data);
     this.setTime();
-    console.log(this.data);
   },
   // 设置服务时间
   setTime() {
@@ -51,11 +62,17 @@ Page({
     let day = globalData.selectDay + "";
     let time = globalData.selectTime;
     let week = globalData.selectWeek;
-    let formatTime = `${day.substring(0, 4)}年${day.substring(
-      4,
-      6
-    )}月${day.substring(6, 8)}日(周${week})${time}`;
-    this.setData({ time: formatTime });
+    let formatTime = "";
+    if (!day || !time || !week) {
+    } else {
+      formatTime = `${day.substring(0, 4)}年${day.substring(
+        4,
+        6
+      )}月${day.substring(6, 8)}日(周${week})${time}`;
+    }
+    this.setData({
+      "formData.time.value": formatTime,
+    });
   },
   // 跳转到日历页面
   goToCalendar() {
@@ -74,9 +91,12 @@ Page({
   },
   bindPickerChange_hx: function (e) {
     // console.log("picker发送选择改变，携带值为", e.detail.value);
+    let value = this.data.photographyType[e.detail.value].title;
+    console.log(value);
     this.setData({
       //给变量赋值
-      hx_index: e.detail.value, //每次选择了下拉列表的内容同时修改下标然后修改显示的内容，显示的内容和选择的内容一致
+      // index: e.detail.value, //每次选择了下拉列表的内容同时修改下标然后修改显示的内容，显示的内容和选择的内容一致
+      "formData.server.value": value,
     });
     // console.log("自定义值:", this.data.hx_select);
   },
@@ -86,5 +106,68 @@ Page({
   bindChange: function (e) {
     var that = this;
     that.setData({ currentTab: e.detail.current });
+  },
+
+  // 立即预约
+  submit() {
+    console.log(this.data);
+    let formData = this.data.formData;
+    for (const key in formData) {
+      if (Object.hasOwnProperty.call(formData, key)) {
+        if (formData[key].value === "") {
+          let isError = `formData.${key}.isError`;
+          console.log(isError);
+          this.setData({
+            [isError]: true,
+          });
+        }
+      }
+    }
+    console.log(this.data.formData);
+  },
+
+  // input失焦
+  inputBlur(event) {
+    console.log(event);
+    let value = event.detail.value;
+    let type = event.target.id;
+    switch (type) {
+      case "name":
+        console.log("name");
+        if (value.length == 0) {
+          this.setData({
+            "formData.name.isError": true,
+            "formData.name.massage": "请输入联系人",
+          });
+        } else {
+          this.setData({
+            "formData.name.isError": false,
+            "formData.name.value": value,
+          });
+        }
+        break;
+      case "phone":
+        console.log("phone");
+        if (value.length == 0) {
+          this.setData({
+            "formData.phone.isError": true,
+            "formData.phone.massage": "请输入联系电话",
+          });
+        } else {
+          let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
+          if (reg.test(value)) {
+            this.setData({
+              "formData.phone.isError": false,
+              "formData.phone.vaule": value,
+            });
+          } else {
+            this.setData({
+              "formData.phone.isError": true,
+              "formData.phone.massage": "请输入正确的号码",
+            });
+          }
+        }
+        break;
+    }
   },
 });
