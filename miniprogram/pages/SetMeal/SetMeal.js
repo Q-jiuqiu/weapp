8b2add5a34d23ab0c037aa05257b2e500e7676f5
quling <quling@dist.com.cn>
2021-03-15 23:29:08 +0800

    完成筛选页面的布局以及显隐,数据的双向绑定已实现,重置功能已实现.

--- miniprogram/pages/SetMeal/SetMeal.js
+++ miniprogram/pages/SetMeal/SetMeal.js
@@ -19,6 +19,18 @@ Component({
     nameList: [],
     defaultArr: [],
     sort: "default",
+    isSearch: false,
+    formData: {},
+    selectList: [],
+    hotSearch: [
+      { search: "证件照", select: false },
+      { search: "结婚登记照", select: false },
+      { search: "形象照", select: false },
+      { search: "情侣", select: false },
+      { search: "婚纱", select: false },
+    ],
+    warningTop: false,
+    warningLow: false,
   },
 
   /**
@@ -44,6 +56,112 @@ Component({
         .catch((err) => {
           console.log(err);
         });
+    },
+    // 防抖
+    Debounce(fn, time) {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(fn, time);
+    },
+    // 筛选面板显&隐
+    searchShow() {
+      let data = this.data;
+      this.setData({ isSearch: !data.isSearch });
+    },
+    // 输入框绑定
+    bindValue(event) {
+      let value = event.detail.value;
+      let type = event.target.id;
+      let formData = this.data.formData;
+      switch (type) {
+        case "keyword":
+          this.Debounce(() => {
+            this.setData({
+              "formData.keyword": value,
+            });
+          }, 800);
+          break;
+        case "low":
+          this.Debounce(() => {
+            // 最低价和最高价校验
+            let isError = false;
+            console.log("low", value * 1 > formData.top * 1);
+            if (value * 1 > formData.top * 1) {
+              isError = true;
+            } else {
+              isError = false;
+            }
+            this.setData({
+              warningLow: isError,
+              warningTop: false,
+              "formData.low": value,
+            });
+          }, 800);
+          break;
+        case "top":
+          this.Debounce(() => {
+            // 最低价和最高价校验
+            let isError = false;
+            console.log("top", value * 1 < formData.low * 1);
+            if (value * 1 < formData.low * 1) {
+              isError = true;
+            } else {
+              isError = false;
+            }
+            this.setData({
+              warningTop: isError,
+              warningLow: false,
+              "formData.top": value,
+            });
+          }, 800);
+          break;
+      }
+    },
+    // 阻止事件冒泡
+    stop() {},
+    // 重置表单
+    reset() {
+      console.log("重置表单");
+      let formData = this.data.formData;
+      let hotSearch = this.data.hotSearch;
+      for (const key in formData) {
+        if (Object.hasOwnProperty.call(formData, key)) {
+          formData[key] = "";
+        }
+      }
+      hotSearch.forEach((item) => {
+        item.select = false;
+      });
+      this.setData({
+        formData,
+        hotSearch,
+        warningTop: false,
+        warningLow: false,
+      });
+    },
+    // 提交表单
+    submit() {},
+    // 选择热门搜索按钮
+    searchClick(event) {
+      let hotSearch = this.data.hotSearch;
+      let selectList = this.data.selectList;
+      let index = event.target.dataset.index;
+      hotSearch[index].select = !hotSearch[index].select;
+      if (hotSearch[index].select) {
+        selectList.push(hotSearch[index].search);
+      } else {
+        let indexOf = selectList.indexOf(hotSearch[index].search);
+        selectList.splice(indexOf, 1);
+      }
+      let selectString = "";
+      selectList.forEach((item) => {
+        selectString = selectString + " " + item;
+      });
+      this.setData({
+        hotSearch,
+        selectList,
+        "formData.keyword": selectString,
+      });
+      console.log(this.data.formData.keyword);
     },
     // 跳转到套系详情
     goToSeries(event) {
@@ -133,6 +251,7 @@ Component({
   lifetimes: {
     attached() {
       this.getDataArr();
+      this.timer = null;
     },
   },
 });
