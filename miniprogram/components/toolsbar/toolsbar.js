9736f299b9f9557000d0cd5efc75a873f262b694
quling <quling@dist.com.cn>
2021-05-21 11:21:17 +0800

    提价

--- miniprogram/components/toolsbar/toolsbar.js
+++ miniprogram/components/toolsbar/toolsbar.js
@@ -1,6 +1,6 @@
 // components/toolsbar/toolsbar.js
-import { getDetail } from "../../utils/event";
 import getUserInfo from "../../utils/getUserInfo";
+import redirectTo from "../../utils/redirectTo";
 const app = getApp();
 
 Component({
@@ -17,66 +17,22 @@ Component({
    */
   data: {
     isShow: false,
-    animation: null,
-    isShowTips: false,
-  },
-
-  lifetimes: {
-    attached() {
-      this.animation = wx.createAnimation({
-        // 动画持续时间，单位ms，默认值 400
-        duration: 1500,
-        timingFunction: "linear", // 延迟多长时间开始
-        delay: 100,
-        transformOrigin: "left top 0",
-        success: function (res) {
-          console.log(res);
-        },
-      });
-    },
   },
   /**
    * 组件的方法列表
    */
   methods: {
     showSubList() {
-      console.log(this.data.isShow);
       this.setData({
         isShow: !this.data.isShow,
       });
-      // this.animation.rotate(360).step();
-      // this.setData({
-      //   //输出动画
-      //   animation: this.animation.export(),
-      // });
     },
     // 客服会话
-    handleContact(event) {
-      console.log(event);
-      // this.triggerEvent("contact", detail);
-    },
+    // handleContact(event) {
+    //   console.log(event);
+    // },
     getMyInfoGoToOrder(e) {
-      let userInfo = getDetail(e).userInfo;
-      if (userInfo) {
-        console.log("ok");
-        getUserInfo(this, app, userInfo);
-        wx.redirectTo({
-          url: "/pages/order/order",
-          success: function (res) {
-            wx.setNavigationBarTitle({
-              title: "预约",
-            });
-          },
-          fail: function () {
-            // fail
-          },
-          complete: function () {
-            // complete
-          },
-        });
-      } else {
-        return;
-      }
+      redirectTo({ url: "/pages/order/order", urlTitle: "预约" });
     },
     // 授权
     getMyInfo() {
@@ -85,20 +41,7 @@ Component({
         app.globalData.isUser = true;
       } else {
         // 跳转到预定页面
-        wx.redirectTo({
-          url: "/pages/My/My",
-          success: function (res) {
-            wx.setNavigationBarTitle({
-              title: "我的",
-            });
-          },
-          fail: function () {
-            // fail
-          },
-          complete: function () {
-            // complete
-          },
-        });
+        redirectTo({ url: "/pages/My/My", urlTitle: "我的" });
       }
     },
   },
